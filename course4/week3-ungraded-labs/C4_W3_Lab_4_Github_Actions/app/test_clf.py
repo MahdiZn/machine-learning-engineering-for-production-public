d0a7efd3d1bafe796956250eb7ea7d587d5932e0
mahdizn <lmahdizouine@gmail.com>
2022-06-15 19:47:43 +0200

    add new classifier with scling

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
@@ -1,6 +1,7 @@
 import pickle
 from main import clf
-
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 def test_accuracy():
 
@@ -14,5 +15,17 @@ def test_accuracy():
     # Compute accuracy of classifier
     acc = clf.score(X_test, y_test)
 
-    # Accuracy should be over OR EQUAL 90%
-    assert acc >= 0.9
+    # Accuracy should be over  90%
+    assert acc > 0.9
+
+def test_pipeline_and_scaler():
+
+    # Check if clf is an instance of sklearn.pipeline.Pipeline 
+    isPipeline = isinstance(clf, Pipeline)
+    assert isPipeline
+    
+    if isPipeline:
+        # Check if first step of pipeline is an instance of 
+        # sklearn.preprocessing.StandardScaler
+        firstStep = [v for v in clf.named_steps.values()][0]
+        assert isinstance(firstStep, StandardScaler)
